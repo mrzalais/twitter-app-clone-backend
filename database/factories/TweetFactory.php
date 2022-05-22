3ac50abec3891e3955f6f6de835290ada20ae268
edijslejnieks <edijslejnieks@flynowpaylater.com>
2022-05-22 22:00:37 +0300

    Move backend to separate repository

---
+++ database/factories/TweetFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tweet>
+ */
+class TweetFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'body' => $this->faker->sentence(20),
+            'created_at' => $this->faker->dateTimeBetween('-30 days', now()),
+        ];
+    }
+}
